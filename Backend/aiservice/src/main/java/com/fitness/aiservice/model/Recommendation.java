748c817a4b73a849e2b085256a5d1c1b19e4438a
Rakesh K <rakeshkraki3272@gmail.com>
2025-12-10 22:37:31 +0530

    AI config

---
+++ Backend/aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -0,0 +1,94 @@
+package com.fitness.aiservice.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "recommendations")
+public class Recommendation {
+
+    @Id
+    private String id;
+    private String userId;
+    private String activityId;
+    private String activityType;
+    private String recommendation;
+    private List<String> improvements;
+    private List<String> suggestions;
+    private List<String> safety;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public Recommendation(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(String activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getActivityType() {
+        return activityType;
+    }
+
+    public void setActivityType(String activityType) {
+        this.activityType = activityType;
+    }
+
+    public String getRecommendation() {
+        return recommendation;
+    }
+
+    public void setRecommendation(String recommendation) {
+        this.recommendation = recommendation;
+    }
+
+    public List<String> getImprovements() {
+        return improvements;
+    }
+
+    public void setImprovements(List<String> improvements) {
+        this.improvements = improvements;
+    }
+
+    public List<String> getSuggestions() {
+        return suggestions;
+    }
+
+    public void setSuggestions(List<String> suggestions) {
+        this.suggestions = suggestions;
+    }
+
+    public List<String> getSafety() {
+        return safety;
+    }
+
+    public void setSafety(List<String> safety) {
+        this.safety = safety;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+}
